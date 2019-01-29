237ff6524040fbecaf955b5d07f49bd73358544f
RED-00 <PSantos211@gmail.com>
2019-01-28 19:09:15 -0800

    initialized maven

---
+++ BudgetBuddy/src/main/java/Budgetapp/App.java
@@ -0,0 +1,13 @@
+package Budgetapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
