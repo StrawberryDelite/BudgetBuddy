faecb702b1c6134600260d7b2557ec0be6110a42
RED-00 <PSantos211@gmail.com>
2019-01-30 20:37:46 -0800

    addStack method

--- src/budgetbuddy/Category.java
+++ src/budgetbuddy/Category.java
@@ -4,32 +4,55 @@ import java.util.Stack;
 
 public class Category {
 	
-	 private Stack<Transaction> history = new Stack<Transaction>();
+	 private Stack<Transaction> transHistory = new Stack<Transaction>();
+	 
+	 
+	 
+	 
+	 public void addStack(Stack<Transaction> NewItems) 
+	 //adds the argument to the transHistory stack
+	 {
+		 while (NewItems.size() > 0)
+		 {
+			 transHistory.push(NewItems.pop());
+		 }
+	 }
+	 
+	 
+	 
 	 
 	 public double expenses()
-	 {
-		 //collates the costs of all Transactions in the category's history Stack
-		 
+	 //collates the costs of all Transactions in the category's transHistory Stack
+	 {	 
 		 double accumulator = 0;
 		 Transaction temp = new Transaction();
 		 Stack<Transaction> tempStack = new Stack<Transaction>();
 		 
 		 
-		 while (history.size() > 0)
+		 while (transHistory.size() > 0)
 		 {
-			 temp = tempStack.push(history.pop());
+			 temp = tempStack.push(transHistory.pop());
 			 accumulator += temp.cost();
 		 }
 		 
 		 while (tempStack.size() > 0)
 		 {
-			 history.push(tempStack.pop());
+			 transHistory.push(tempStack.pop());
 		 }
 		 
 		 return accumulator;
 	 }
 	 
-	 
+	 public Transaction[] history(int length) // TODO
+	 //returns a specified number of transactions in an array, with the most recent in index 0
+	 {
+		 
+		 Transaction[] transArray = new Transaction[length];
+		 
+		 
+		 
+		 return transArray;
+	 }
 	 
 	 
 }
